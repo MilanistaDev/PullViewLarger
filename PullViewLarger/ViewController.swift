99605e431b9bb888d40c3074c72afa7c1d287ed0
Takuya Aso <milanista224@gmail.com>
2020-04-06 23:32:29 +0900

    Implemented upper UI parts.

--- PullViewLarger/ViewController.swift
+++ PullViewLarger/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var profileImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setUpUI()
+        self.tableView.bringSubviewToFront(self.profileView)
     }
 
+    private func setUpUI() {
+        self.tableView.bringSubviewToFront(self.profileView)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+        self.profileImageView.layer.borderColor = UIColor.white.cgColor
+        self.profileImageView.layer.borderWidth = 2.0
+    }
 
 }
 
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
